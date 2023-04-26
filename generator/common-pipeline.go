185819cdfef662773361d8d2977f9216ac2f70c2
hnadiminti-equinix <hari.nadiminti@eu.equinix.com>
2023-04-26 18:14:08 +0100

    Updated common pipeline and created trivy templates

--- generator/common-pipeline.go
+++ generator/common-pipeline.go
@@ -1,33 +1,41 @@
 package generator
 
 import (
-	"fmt"
 	"io"
+	"strings"
+	"text/template"
 )
+
+// Generator is the struct to keep the values passed to the trivy step template
+type Generator struct {
+	TrivyPlugin    string
+	ShellPlugin    string
+	Severity       []string
+	IgnoreUnfixed  bool
+	SecurityChecks []string
+	SkipFiles      string
+}
 
 // GenerateTrivyStep takes trivy plugin version and shell plugin version
 // and an io.Writer to generate trivy step configuration. The trivy step is
 // written to the provided io.Writer.
 // It returns error in case write to the io.Writer errors out.
 func GenerateTrivyStep(trivyPlugin, shellPlugin string, w io.Writer) error {
-	trivyStepFormat := `
-steps:
-- command: ls
-  plugins:
-    - equinixmetal-buildkite/trivy#%s
-	    severity: CRITICAL,HIGH
-	    ignore-unfixed: true
-	    security-checks: config,secret,vuln
-	    skip-files: 'cosign.key'
-- label: ":sparkles: SHELL CHECK"
-  plugins:
-    - shellcheck#%s:
-        files: script.sh
-`
-
-	trivyStep := fmt.Sprintf(trivyStepFormat, trivyPlugin, shellPlugin)
-	if _, err := w.Write([]byte(trivyStep)); err != nil {
-		return fmt.Errorf("error writing trivy step to the output stream: %w", err)
+	generator := Generator{
+		TrivyPlugin:    trivyPlugin,
+		ShellPlugin:    shellPlugin,
+		Severity:       []string{"CRITICAL", "HIGH"},
+		IgnoreUnfixed:  true,
+		SecurityChecks: []string{"config", "secret", "vuln"},
+		SkipFiles:      "cosign.key",
 	}
-	return nil
+
+	funcMap := template.FuncMap{
+		"join": func(arr []string) string {
+			return strings.Join(arr, ",")
+		},
+	}
+
+	tpl := template.Must(template.New("").Funcs(funcMap).ParseGlob("templates/*"))
+	return tpl.ExecuteTemplate(w, "trivy-step.tmpl", generator)
 }
