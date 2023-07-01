7a7de415ed145a753e74cee8e85e30556873c740
hnadiminti-equinix <hari.nadiminti@eu.equinix.com>
2023-07-01 06:26:25 +0100

    updte main.go

--- main.go
+++ main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"dynamic-buildkite-template/cmd"
+	"dynamic-buildkite-template/config"
+	"os"
 )
 
+func main() {
+	cmd.Execute()
+}
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
+
+	// Only log the error severity or above.
+	log.SetLevel(log.ErrorLevel)
+
+	if err := config.LoadConfig("resources/config"); err != nil {
+		panic(err)
+	}
+}
 func main() {
 	cmd.Execute()
 }
