1dd2fa2bada8255a94838fa0690eb399316cab2a
hnadiminti-equinix <hari.nadiminti@eu.equinix.com>
2024-02-07 13:07:50 +0000

    removed the unnecessary comment lines

--- cmd/command.go
+++ cmd/command.go
@@ -4,18 +4,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// var (
-// 	commandConfig generator.CommandConfig
-// )
-
 // LoadCosignConfigs loads cosign plugin configuration from conf.yaml using "plugins.cosign" key
 func LoadCommandConfigs(cmd *cobra.Command) {
-
-	//g.CommandConfig.CommandConfig = "ls"
 	m, _ := cmd.Flags().GetStringToString("overrides")
 	for k, v := range m {
 		if k == "command" {
-			//fmt.Println("command key found!!!!", v)
 			g.CommandConfig.CommandConfig = v
 			g.CommandConfigEnable = true
 		}
